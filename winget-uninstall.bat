386f3a7d774945d1012178f6ac1223501b5c96aa
Iain <netean@gmail.com>
2025-02-24 18:35:42 +0000

    Add files via upload

---
+++ winget-uninstall.bat
@@ -0,0 +1,10 @@
+
+## Run as admin
+
+winget uninstall slack  		 
+winget uninstall clickup  	  
+winget uninstall 7zip  		  
+winget uninstall peazip  	
+winget uninstall powertoys	 
+winget uninstall rustdesk  		 
+winget uninstall anydesk.anydesk  		  
