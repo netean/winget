7d55e56adfee6d0f3b2d88d95b32da6f5d9a0dd0
Iain <netean@gmail.com>
2025-02-24 19:38:53 +0000

    Added Devops tools mostly from

---
+++ Winget-Devops-install.ps1
@@ -0,0 +1,41 @@
+## run in administrator terminal
+
+## Git
+winget install -e --id Git.Git
+winget install -e --id TortoiseGit.TortoiseGit
+
+
+
+## ides & code
+winget install -e --id Microsoft.VisualStudioCode
+## Install "latest Python"
+winget install python3
+
+## winmerge
+winget install -e --id WinMerge.WinMerge
+
+## Neovim
+## winget install -e --id Neovim.Neovim
+
+
+## SSH
+winget install -e --id PuTTY.PuTTY
+
+## Helm for Kubernetes
+winget install Helm.Helm
+
+
+## Azure cli
+winget install -e --id Microsoft.AzureCLI
+
+## Docker Desktop
+winget install -e --id Docker.DockerDesktop
+
+
+
+### ==========================
+## optional, uncomment to run
+### ==========================
+
+## install ubuntu in wsl2
+## winget install --id=Microsoft.WSL  -e
