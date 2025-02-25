970ae39498bd7b8a434c9a8a6158b485438449e8
Iain <netean@gmail.com>
2025-02-25 11:37:10 +0000

    Update Winget-install.bat
    Updated packagesd, chrome, OnlyOffice, zoome Notepad ++ Paint.net

--- Winget-install.bat
+++ Winget-install.bat
@@ -3,11 +3,16 @@
 ::## 
 
 
-winget install slack  		        --accept-package-agreements --accept-source-agreements --source=msstore
-winget install clickup  	        --accept-package-agreements --accept-source-agreements --source=msstore
+winget install slack  		      --accept-package-agreements --accept-source-agreements --source=msstore
+winget install clickup  	      --accept-package-agreements --accept-source-agreements --source=msstore
 winget install 7zip  		        --accept-package-agreements --accept-source-agreements --source=msstore
-winget install peazip  		        --accept-package-agreements --accept-source-agreements --source=msstore
-winget install powertoys	        --accept-package-agreements --accept-source-agreements --source=msstore
+winget install peazip  		      --accept-package-agreements --accept-source-agreements --source=msstore
+winget install powertoys	      --accept-package-agreements --accept-source-agreements --source=msstore
 winget install rustdesk  		    --accept-package-agreements --accept-source-agreements 
-winget install anydesk.anydesk      --accept-package-agreements --accept-source-agreements 
-winget install notion               --accept-package-agreements --accept-source-agreements --source=msstore
+winget install anydesk.anydesk  --accept-package-agreements --accept-source-agreements 
+winget install google.chrome  	 --accept-package-agreements --accept-source-agreements 
+winget install notepad++	       --accept-package-agreements --accept-source-agreements
+winget install ONLYOFFICE.DesktopEditors		--accept-package-agreements --accept-source-agreements
+winget install zoom		            --accept-package-agreements --accept-source-agreements --source=msstore
+winget install paint.net  	      --accept-package-agreements --accept-source-agreements --source=winget
+winget install Google.CredentialProviderForWindows  --accept-package-agreements --accept-source-agreements --source=winget
